429250a797d1cbce6b34a1d59d8784e970fdeb0f
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-05-26 12:35:56 +0400

    completed side bar - no dark mode functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,96 @@
+import { Group } from '@mui/icons-material';
+import { Article } from '@mui/icons-material';
+import { Storefront } from '@mui/icons-material';
+import { Settings } from '@mui/icons-material';
+import { ModeNight } from '@mui/icons-material';
+import { AccountBox } from '@mui/icons-material';
+import { Person } from '@mui/icons-material';
+import { Home } from '@mui/icons-material';
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material';
 import Box from '@mui/material/Box';
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor='skyblue' flex={1} padding={2} sx={{display: {xs: 'none', sm: 'block' }}}>Sidebar</Box>
+    <Box 
+    flex={1} 
+    padding={2} 
+    sx={{display: {xs: 'none', sm: 'block' }}}
+    >
+    <List>
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#Home'>
+              <ListItemIcon>
+               <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <Article />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <Group />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <Storefront />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <Person />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+    </List>
+
+    <ListItem disablePadding>
+            <ListItemButton Component='a' href='#simple-list'>
+              <ListItemIcon>
+               <ModeNight />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+    </Box>
   )
 }
 
